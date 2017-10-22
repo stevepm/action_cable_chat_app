beba0418fc4b762630b9dd9b59245ae32d4ad0c7
Steven Magelowitz <steven.magelowitz@gmail.com>
2017-10-22 15:16:03 -0700

    changes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,11 +12,17 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: cookies.signed[:user_id])
+    @current_user ||= User.find_by(id: user_id)
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  private
+
+  def user_id
+    cookies.signed[:user_id]
   end
 end
